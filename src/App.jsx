edf0bba01e7c45000bf7225d663616eec0274dab
Melvin A <melvinalmonte@gmail.com>
2022-03-01 01:33:58 -0500

    landscape alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
+import React from "react";
 import { Wizard } from "./components/wizard/Wizard";
+import { Box, Container, Fade, Heading } from "@chakra-ui/react";
 
 function App() {
-  return <Wizard />;
+  const [orientation, setOrientation] = React.useState(
+    !navigator.maxTouchPoints
+      ? "desktop"
+      : !window.screen.orientation.angle
+      ? "portrait"
+      : "landscape"
+  );
+
+  React.useEffect(() => {
+    window.addEventListener("resize", detectOrientation);
+  }, [orientation]);
+
+  const detectOrientation = () => {
+    if (Window.orientation === 90 || Window.orientation === -90) {
+      setOrientation("landscape");
+    } else {
+      setOrientation(
+        !navigator.maxTouchPoints
+          ? "desktop"
+          : !window.screen.orientation.angle
+          ? "portrait"
+          : "landscape"
+      );
+    }
+  };
+  return (
+    <>
+      {orientation === "landscape" ? (
+        <Box
+          width={"100%"}
+          maxWidth={"500px"}
+          margin={"0 auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          height={"100%"}
+        >
+          <Container
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexGrow={1}
+            overflow={"auto"}
+          >
+            <Fade in>
+              <Heading fontSize={"xl"} color={"#00ADB5"}>
+                Landscape mode is not supported.
+              </Heading>
+            </Fade>
+          </Container>
+        </Box>
+      ) : (
+        <Wizard />
+      )}
+    </>
+  );
 }
 
 export default App;
