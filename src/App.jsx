8321876be7932a4c10326a780e93e9dec351e069
Melvin A <melvinalmonte@gmail.com>
2022-03-01 09:54:15 -0500

    troubleshooting orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { Wizard } from "./components/wizard/Wizard";
-import { Box, Container, Text } from "@chakra-ui/react";
+// import { Wizard } from "./components/wizard/Wizard";
+import {
+  // Box,
+  // Container,
+  Text
+} from "@chakra-ui/react";
 
 function App() {
   const [orientation, setOrientation] = React.useState("");
@@ -11,41 +15,43 @@ function App() {
 
   const detectOrientation = () => {
     const isMobile = localStorage.mobile || window.navigator.maxTouchPoints > 1;
-    if (isMobile) {
-      if (screen.availHeight < screen.availWidth) {
-        setOrientation("landscape");
-      } else {
-        setOrientation("portrait");
-      }
-    }
+    setOrientation(isMobile);
+    // if (isMobile) {
+    //   if (screen.availHeight < screen.availWidth) {
+    //     setOrientation("landscape");
+    //   } else {
+    //     setOrientation("portrait");
+    //   }
+    // }
   };
 
   return (
     <>
-      {orientation === "landscape" ? (
-        <Box
-          width={"100%"}
-          maxWidth={"500px"}
-          margin={"0 auto"}
-          display={"flex"}
-          flexDirection={"column"}
-          height={"100%"}
-        >
-          <Container
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            flexGrow={1}
-            overflow={"auto"}
-          >
-            <Text color={"#00ADB5"}>
-              Landscape mode is currently not supported.
-            </Text>
-          </Container>
-        </Box>
-      ) : (
-        <Wizard />
-      )}
+      <Text>{JSON.stringify({ isMobile: orientation })}</Text>
+      {/*{orientation === "landscape" ? (*/}
+      {/*  <Box*/}
+      {/*    width={"100%"}*/}
+      {/*    maxWidth={"500px"}*/}
+      {/*    margin={"0 auto"}*/}
+      {/*    display={"flex"}*/}
+      {/*    flexDirection={"column"}*/}
+      {/*    height={"100%"}*/}
+      {/*  >*/}
+      {/*    <Container*/}
+      {/*      display={"flex"}*/}
+      {/*      justifyContent={"center"}*/}
+      {/*      alignItems={"center"}*/}
+      {/*      flexGrow={1}*/}
+      {/*      overflow={"auto"}*/}
+      {/*    >*/}
+      {/*      <Text color={"#00ADB5"}>*/}
+      {/*        Landscape mode is currently not supported.*/}
+      {/*      </Text>*/}
+      {/*    </Container>*/}
+      {/*  </Box>*/}
+      {/*) : (*/}
+      {/*  <Wizard />*/}
+      {/*)}*/}
     </>
   );
 }
