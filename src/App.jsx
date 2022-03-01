b9c6e6f9b799c2a85d48465c5bf52dcb4b90a266
Melvin A <melvinalmonte@gmail.com>
2022-03-01 10:11:55 -0500

    troubleshooting orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { Wizard } from "./components/wizard/Wizard";
-import { Box, Container, Text } from "@chakra-ui/react";
+// import { Wizard } from "./components/wizard/Wizard";
+import {
+  // Box,
+  // Container,
+  Text } from "@chakra-ui/react";
 
 function App() {
   const [orientation, setOrientation] = React.useState("");
@@ -11,47 +14,56 @@ function App() {
 
   const detectOrientation = () => {
     const isMobile = localStorage.mobile || window.navigator.maxTouchPoints > 1;
+    const isSafari =
+      navigator.vendor.match(/apple/i) &&
+      !navigator.userAgent.match(/crios/i) &&
+      !navigator.userAgent.match(/fxios/i) &&
+      !navigator.userAgent.match(/Opera|OPT\//);
+
     if (isMobile) {
-      if (window.outerHeight === 0) {
-        if (window.outerWidth > window.outerHeight) {
-          return setOrientation("portrait");
-        }
-        return setOrientation("landscape");
+      if (isSafari) {
+        setOrientation(isSafari);
+        // if (window.outerWidth > window.outerHeight) {
+        //   return setOrientation("portrait");
+        // }
+        // return setOrientation("landscape");
       } else {
-        if (window.outerWidth > window.outerHeight) {
-          return setOrientation("landscape");
-        }
-        return setOrientation("portrait");
+        setOrientation(isSafari);
+        // if (window.outerWidth > window.outerHeight) {
+        //   return setOrientation("landscape");
+        // }
+        // return setOrientation("portrait");
       }
     }
   };
 
   return (
     <>
-      {orientation === "landscape" ? (
-        <Box
-          width={"100%"}
-          maxWidth={"500px"}
-          margin={"0 auto"}
-          display={"flex"}
-          flexDirection={"column"}
-          height={"100%"}
-        >
-          <Container
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-            flexGrow={1}
-            overflow={"auto"}
-          >
-            <Text color={"#00ADB5"}>
-              Landscape mode is currently not supported.
-            </Text>
-          </Container>
-        </Box>
-      ) : (
-        <Wizard />
-      )}
+      <Text>{JSON.stringify({ isSafari: orientation }, null, 2)}</Text>
+      {/*{orientation === "landscape" ? (*/}
+      {/*  <Box*/}
+      {/*    width={"100%"}*/}
+      {/*    maxWidth={"500px"}*/}
+      {/*    margin={"0 auto"}*/}
+      {/*    display={"flex"}*/}
+      {/*    flexDirection={"column"}*/}
+      {/*    height={"100%"}*/}
+      {/*  >*/}
+      {/*    <Container*/}
+      {/*      display={"flex"}*/}
+      {/*      justifyContent={"center"}*/}
+      {/*      alignItems={"center"}*/}
+      {/*      flexGrow={1}*/}
+      {/*      overflow={"auto"}*/}
+      {/*    >*/}
+      {/*      <Text color={"#00ADB5"}>*/}
+      {/*        Landscape mode is currently not supported.*/}
+      {/*      </Text>*/}
+      {/*    </Container>*/}
+      {/*  </Box>*/}
+      {/*) : (*/}
+      {/*  <Wizard />*/}
+      {/*)}*/}
     </>
   );
 }
