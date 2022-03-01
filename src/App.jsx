32a95a2168fda07829661818d8fa5d9d2793b44c
Melvin A <melvinalmonte@gmail.com>
2022-03-01 09:51:13 -0500

    troubleshooting orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import { Wizard } from "./components/wizard/Wizard";
+import { Box, Container, Text } from "@chakra-ui/react";
 
 function App() {
-  return <Wizard />;
+  const [orientation, setOrientation] = React.useState("");
+
+  React.useEffect(() => {
+    window.addEventListener("resize", detectOrientation);
+  }, [orientation]);
+
+  const detectOrientation = () => {
+    const isMobile = localStorage.mobile || window.navigator.maxTouchPoints > 1;
+    if (isMobile) {
+      if (screen.availHeight < screen.availWidth) {
+        setOrientation("landscape");
+      } else {
+        setOrientation("portrait");
+      }
+    }
+  };
+
+  return (
+    <>
+      {orientation === "landscape" ? (
+        <Box
+          width={"100%"}
+          maxWidth={"500px"}
+          margin={"0 auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          height={"100%"}
+        >
+          <Container
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexGrow={1}
+            overflow={"auto"}
+          >
+            <Text color={"#00ADB5"}>
+              Landscape mode is currently not supported.
+            </Text>
+          </Container>
+        </Box>
+      ) : (
+        <Wizard />
+      )}
+    </>
+  );
 }
 
 export default App;
