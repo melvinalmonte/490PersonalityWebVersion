b07452fd38754087e3efd39a9b0081e542c22eba
Melvin A <melvinalmonte@gmail.com>
2022-03-01 02:40:37 -0500

    troubleshooting orientation

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,46 @@ function App() {
   const [orientation, setOrientation] = React.useState("");
 
   React.useEffect(() => {
-    window.addEventListener("change", detectOrientation);
+    screen.orientation.addEventListener("change", detectOrientation);
   }, [orientation]);
 
   const detectOrientation = (e) => {
-    setOrientation(e.currentTarget.type);
+    if (
+      e.currentTarget.type === "landscape-primary" ||
+      e.currentTarget.type === "landscape-secondary"
+    ) {
+      setOrientation("landscape");
+    } else {
+      setOrientation("portrait");
+    }
   };
 
   return (
     <>
-      {console.log(JSON.stringify({ orientation: orientation }, null, 2))}
-      {/*{orientation === "landscape" ? (*/}
-      {/*  <Box*/}
-      {/*    width={"100%"}*/}
-      {/*    maxWidth={"500px"}*/}
-      {/*    margin={"0 auto"}*/}
-      {/*    display={"flex"}*/}
-      {/*    flexDirection={"column"}*/}
-      {/*    height={"100%"}*/}
-      {/*  >*/}
-      {/*    <Container*/}
-      {/*      display={"flex"}*/}
-      {/*      justifyContent={"center"}*/}
-      {/*      alignItems={"center"}*/}
-      {/*      flexGrow={1}*/}
-      {/*      overflow={"auto"}*/}
-      {/*    >*/}
-      {/*      <Text color={"#00ADB5"}>*/}
-      {/*        Landscape mode is currently not supported.*/}
-      {/*      </Text>*/}
-      {/*    </Container>*/}
-      {/*  </Box>*/}
-      {/*) : (*/}
-      {/*  <Wizard />*/}
-      {/*)}*/}
+      {orientation === "landscape" ? (
+        <Box
+          width={"100%"}
+          maxWidth={"500px"}
+          margin={"0 auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          height={"100%"}
+        >
+          <Container
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexGrow={1}
+            overflow={"auto"}
+          >
+            <Text color={"#00ADB5"}>
+              Landscape mode is currently not supported.
+            </Text>
+          </Container>
+        </Box>
+      ) : (
+        <Wizard />
+      )}
     </>
   );
 }
