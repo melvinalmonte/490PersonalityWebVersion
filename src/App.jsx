0f73cc9f21ef2592da955ec4742a7f9bbc2f74e9
Melvin A <melvinalmonte@gmail.com>
2022-03-01 10:02:45 -0500

    troubleshooting orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-// import { Wizard } from "./components/wizard/Wizard";
-import {
-  // Box,
-  // Container,
-  Text,
-} from "@chakra-ui/react";
+import { Wizard } from "./components/wizard/Wizard";
+import { Box, Container, Text } from "@chakra-ui/react";
 
 function App() {
   const [orientation, setOrientation] = React.useState("");
@@ -25,31 +21,30 @@ function App() {
 
   return (
     <>
-      <Text>{JSON.stringify({ isMobile: orientation })}</Text>
-      {/*{orientation === "landscape" ? (*/}
-      {/*  <Box*/}
-      {/*    width={"100%"}*/}
-      {/*    maxWidth={"500px"}*/}
-      {/*    margin={"0 auto"}*/}
-      {/*    display={"flex"}*/}
-      {/*    flexDirection={"column"}*/}
-      {/*    height={"100%"}*/}
-      {/*  >*/}
-      {/*    <Container*/}
-      {/*      display={"flex"}*/}
-      {/*      justifyContent={"center"}*/}
-      {/*      alignItems={"center"}*/}
-      {/*      flexGrow={1}*/}
-      {/*      overflow={"auto"}*/}
-      {/*    >*/}
-      {/*      <Text color={"#00ADB5"}>*/}
-      {/*        Landscape mode is currently not supported.*/}
-      {/*      </Text>*/}
-      {/*    </Container>*/}
-      {/*  </Box>*/}
-      {/*) : (*/}
-      {/*  <Wizard />*/}
-      {/*)}*/}
+      {orientation === "landscape" ? (
+        <Box
+          width={"100%"}
+          maxWidth={"500px"}
+          margin={"0 auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          height={"100%"}
+        >
+          <Container
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexGrow={1}
+            overflow={"auto"}
+          >
+            <Text color={"#00ADB5"}>
+              Landscape mode is currently not supported.
+            </Text>
+          </Container>
+        </Box>
+      ) : (
+        <Wizard />
+      )}
     </>
   );
 }
