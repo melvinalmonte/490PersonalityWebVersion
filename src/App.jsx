9604614b9b1b83809f9d9dbe9dd01c7174de24ff
Melvin A <melvinalmonte@gmail.com>
2022-03-01 02:26:10 -0500

    troubleshooting orientation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import React from "react";
-// import { Wizard } from "./components/wizard/Wizard";
-import { Text } from "@chakra-ui/react";
+import { Wizard } from "./components/wizard/Wizard";
+import { Box, Container, Text } from "@chakra-ui/react";
 
 function App() {
   const [orientation, setOrientation] = React.useState("");
 
   React.useEffect(() => {
-    window.addEventListener("resize", detectOrientation);
-  }, [orientation]);
+    window.addEventListener("orientationchange", detectOrientation);
+  }, []);
 
   const detectOrientation = () => {
-    if (window.outerWidth > window.outerHeight) {
-      return setOrientation("landscape");
+    if (screen.availHeight < screen.availWidth) {
+      setOrientation("landscape");
+    } else {
+      setOrientation("portrait");
     }
-    return setOrientation("portrait");
   };
 
   return (
     <>
-      <Text>{JSON.stringify({ orientation: orientation }, null, 2)}</Text>
-      {/*<Wizard />*/}
+      {orientation === "landscape" ? (
+        <Box
+          width={"100%"}
+          maxWidth={"500px"}
+          margin={"0 auto"}
+          display={"flex"}
+          flexDirection={"column"}
+          height={"100%"}
+        >
+          <Container
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexGrow={1}
+            overflow={"auto"}
+          >
+            <Text color={"#00ADB5"}>
+              Landscape mode is currently not supported.
+            </Text>
+          </Container>
+        </Box>
+      ) : (
+        <Wizard />
+      )}
     </>
   );
 }
